3a1fafdff276b4bb15ea87dcacd3a88320d50cee
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2016-07-15 19:19:46 -0300

    Exercício 2.14 Modificando o Hello World

--- MeuPrograma.java
+++ MeuPrograma.java
@@ -1,5 +1,5 @@
 class MeuPrograma {
 	public static void main(String[] args) {
-		System.out.println("Minha primeira aplicação Java!");
+		System.out.println("uma mensagem diferente\numa outra mensagem diferente");
 	}
 }
