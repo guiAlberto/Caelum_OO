8a225c8edd6bbd65660a0c8e9d1952786dc6561d
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2016-07-16 00:56:31 -0300

    Exercício 3.14 Desafio da série de Fibonacci: Faça o exercício da série de Fibonacci usando apenas duas variáveis

--- Exercicio6.java
+++ Exercicio6.java
@@ -2,14 +2,12 @@ class Exercicio6{
 	public static void main(String[] args){
 		int primeiro = 0;
 		int segundo = 1;
-		int valor = primeiro + segundo;
 		System.out.println(primeiro);
 		System.out.println(segundo);
 		do{
-			System.out.println(valor);
-			valor = primeiro + segundo;
-			primeiro = segundo;
-			segundo = valor;		
-		}while(valor <= 100);
+			System.out.println(primeiro + segundo);
+			segundo = primeiro + segundo;
+			primeiro = segundo - primeiro;
+		}while(primeiro + segundo <= 100);
 	}
 }
