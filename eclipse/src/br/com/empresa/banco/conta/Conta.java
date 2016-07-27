65b1a875f3136406cb6ae2a018a54f3130ca56f9
guiAlberto <g.alberto.moraes@gmail.com>
2016-07-27 19:30:32 -0300

    Exercício 13.5
    Jar e Javadoc

--- eclipse/src/br/com/empresa/banco/conta/Conta.java
+++ eclipse/src/br/com/empresa/banco/conta/Conta.java
@@ -1,9 +1,21 @@
 package br.com.empresa.banco.conta;
 
+/**
+ * Classe responsável por moldar contas de banco
+ * 
+ * @author guilherme
+ *
+ */
+
 public abstract class Conta {
 
 	protected double saldo;
 
+	/**
+	 * Método que incrementa algum valor maior do que zero ao saldo da conta
+	 * 
+	 * @param valor
+	 */
 	public void deposita(double valor) {
 		if (valor < 0) {
 			throw new ValorInvalidoException(valor);
@@ -12,10 +24,19 @@ public abstract class Conta {
 		}
 	}
 
+	/**
+	 * Método que retira algm valor da conta
+	 *  
+	 * @param valor
+	 */
 	public void saca(double valor) {
 		this.saldo -= valor;
 	}
 
+	/**
+	 * getter do atributo saldo
+	 * @return
+	 */
 	public double getSaldo() {
 		return this.saldo;
 	}
@@ -24,8 +45,18 @@ public abstract class Conta {
 	// this.saldo += this.saldo * taxa;
 	// }
 
+	/**
+	 * Assinatura do método responsável por aualizar a conta
+	 * 
+	 * @param taxa
+	 */
 	public abstract void atualiza(double taxa);
 
+	/**
+	 * setter do atributo saldo
+	 * 
+	 * @param saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
